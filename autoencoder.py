f6cfaa3e9fccc48ee71fd865b80399292c546677
Tim Ogden <togden1999@gmail.com>
2019-01-31 15:27:39 -0500

    Formatting data

--- autoencoder.py
+++ autoencoder.py
@@ -15,6 +15,8 @@ import numpy as np
 
 def build_model(middle_nodes = 3):
     model = keras.models.Sequential([
+        Input(shape=(28,28)),
+        Flatten(),
         Dense(784, activation='relu', input_shape=(784,)),
         Dense(128, activation='relu'),
         Dense(64, activation='relu'),
@@ -27,49 +29,58 @@ def build_model(middle_nodes = 3):
     return model
     
 
+def import_data():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# In[ ]:
+def getNoisyData(x_train, x_test):
+    noisy_x_train = addNoise(x_train)
+    noisy_x_test = addNoise(x_test)
+    #noisy_x_train = [np.reshape(val,(784,)) for val in noisy_x_train]
+    #noisy_x_test = [np.reshape(val,(784,)) for val in noisy_x_test]
+    return (noisy_x_train, noisy_x_test)
+    
+def getRegularData():
+    #x_train = [np.reshape(val,(784,)) for val in x_train]
+    #x_test = [np.reshape(val,(784,)) for val in x_test]
+    (x_train, _), (x_test, _) = mnist.load_data()
+    return (x_train[:int(.5*len(x_train))], x_test)
 
 
 def addNoise(images, multiplier=2):
     new_images = []
+    i = 0
     for image in images:
         new_image = np.array(image)
         noise = np.random.normal(0,multiplier,784)
+        
         noise = np.reshape(noise, (28,28))
         for r in range(28):
             for c in range(28):
                 new_image[r][c] += noise[r][c]
         new_images.append(new_image)
+        i+=1
+        if i % 2000 == 0:
+            print(i,'/',len(images))
     return new_images
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-def import_data():
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-def getNoisyData():
-    noisy_x_train = addNoise(x_train)
-    noisy_x_test = addNoise(x_test)
-    noisy_x_train = [np.reshape(val,(784,)) for val in noisy_x_train]
-    noisy_x_test = [np.reshape(val,(784,)) for val in noisy_x_test]
-    return (noisy_x_train, noisy_x_test)
-    
-def getRegularData():
-    x_train = [np.reshape(val,(784,)) for val in x_train]
-    x_test = [np.reshape(val,(784,)) for val in x_test]
-    return (x_train, x_test)
+
 
 
 # In[ ]:
 
 
 model = build_model(middle_nodes=28)
-noisy_x_train, noisy_x_test = getNoisyData()
-x_train, x_test = getRegularData()
-try:
-    model.fit(x=noisy_x_train, y=x_train, epochs=10, verbose=2, validation_data=(noisy_x_test, x_test))
-except Exception as e:
-    print(e)
-    pass
+print('model built')
+(x_train, x_test) = getRegularData()
+print('did it just become 0?', len(x_train))
+print('regular data aquired')
+
+(noisy_x_train, noisy_x_test) = getNoisyData(x_train, x_test)
+print('noisy data aquired')
+print('noisy data len:', len(noisy_x_train))
+print('reg data len:', len(x_train))
+model.fit(x=noisy_x_train, y=x_train, batch_size=500, epochs=10, verbose=2, validation_data=(noisy_x_test, x_test))
+
 
